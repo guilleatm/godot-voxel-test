f89adb7eeda665c300a6e51befa3455520e627a0
guilleatm <guillearnautm@gmail.com>
2023-12-16 10:12:04 +0000

    copy paste functions

--- Scripts/test_script.gd
+++ Scripts/test_script.gd
@@ -4,6 +4,7 @@ class_name test_script extends Node3D
 @export var camera : Camera3D
 
 var vt: VoxelTool
+var buffer : VoxelBuffer = VoxelBuffer.new();
 
 func _ready():
 	vt = terrain.get_voxel_tool()
@@ -19,48 +20,81 @@ func _ready():
 
 func _unhandled_input(event):
 	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_SPACE:
-			test()
-		if event.pressed and event.keycode == KEY_R:
-			test2()
-
-
-func test():
-
-	var origin : Vector3i = Vector3i(global_position);
-
-	var buffer : VoxelBuffer = VoxelBuffer.new();
-	var channels_mask : int = 1 << VoxelBuffer.CHANNEL_SDF;
-
-	vt.copy(origin, buffer, channels_mask);
-
-	var offset : Vector3i = Vector3i.ONE * 4;
-
-	vt.paste(origin + offset, buffer, channels_mask);
-
-
-func test2():
+#		if event.pressed and event.keycode == KEY_SPACE:
+#			test()
+#		if event.pressed and event.keycode == KEY_R:
+#			test2()
+		if event.pressed and event.keycode == KEY_C:
+			my_copy()
+		if event.pressed and event.keycode == KEY_V:
+			my_paste()
+	
+	
+	
+func my_copy() -> void:
 	var forward : Vector3 = -camera.basis.z.normalized();
 	
 	var max_distance : float = 10000.0;
 	var raycast_result : VoxelRaycastResult = vt.raycast(global_position, forward, max_distance);
 
-	if (raycast_result == null):
-		print("Raycast from " + str(global_position) + " dir: " + str(forward) + " is null.");
-	
-	else:
-		print("Raycast from " + str(global_position) + " dir: " + str(forward) + " hit voxel " + str(raycast_result.position));
+	if (raycast_result != null):
+		buffer.create(terrain.mesh_block_size, terrain.mesh_block_size, terrain.mesh_block_size);
 
-		var buffer : VoxelBuffer = VoxelBuffer.new();
+		var channels_mask : int = 1 << VoxelBuffer.CHANNEL_SDF;
+		# var channels_mask : int = 0xff;
+		vt.copy(raycast_result.position, buffer, channels_mask);
+		
+		print("Buffer of size " + str(buffer.get_size()) + " copied from voxel " + str(raycast_result.position));
+	
+	
+func my_paste() -> void:
+	var forward : Vector3 = -camera.basis.z.normalized();
+	
+	var max_distance : float = 10000.0;
+	var raycast_result : VoxelRaycastResult = vt.raycast(global_position, forward, max_distance);
+
+	if (raycast_result != null):
 		var channels_mask : int = 1 << VoxelBuffer.CHANNEL_SDF;
 		# var channels_mask : int = 0xff;
 
-		vt.copy(raycast_result.position, buffer, channels_mask);
+		vt.paste(raycast_result.position, buffer, channels_mask);
+		
+		print("Buffer of size " + str(buffer.get_size()) + " pasted to voxel " + str(raycast_result.position));
 
-		var offset : Vector3i = Vector3i.RIGHT * 4;
-
-		vt.paste(raycast_result.position + offset, buffer, channels_mask);
-	
-
-
+#func test2():
+#	var forward : Vector3 = -camera.basis.z.normalized();
+#
+#	var max_distance : float = 10000.0;
+#	var raycast_result : VoxelRaycastResult = vt.raycast(global_position, forward, max_distance);
+#
+#	if (raycast_result == null):
+#		print("Raycast from " + str(global_position) + " dir: " + str(forward) + " is null.");
+#
+#	else:
+#		print("Raycast from " + str(global_position) + " dir: " + str(forward) + " hit voxel " + str(raycast_result.position));
+#
+#		var buffer : VoxelBuffer = VoxelBuffer.new();
+#		buffer.create(terrain.mesh_block_size, terrain.mesh_block_size, terrain.mesh_block_size);
+#		var channels_mask : int = 1 << VoxelBuffer.CHANNEL_SDF;
+#		# var channels_mask : int = 0xff;
+#
+#		vt.copy(raycast_result.position, buffer, channels_mask);
+#
+#		var offset : Vector3i = Vector3i.RIGHT * 4;
+#
+#		vt.paste(raycast_result.position + offset, buffer, channels_mask);
+#
+#func test():
+#
+#	var origin : Vector3i = Vector3i(global_position);
+#
+#	var buffer : VoxelBuffer = VoxelBuffer.new();
+#	var channels_mask : int = 1 << VoxelBuffer.CHANNEL_SDF;
+#
+#	vt.copy(origin, buffer, channels_mask);
+#
+#	var offset : Vector3i = Vector3i.ONE * 4;
+#
+#	vt.paste(origin + offset, buffer, channels_mask);
+#
 
