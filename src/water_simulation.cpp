2471e958170f8b1a5b081c6fa8b5e97b0654404b
guilleatm <guillearnautm@gmail.com>
2024-03-04 14:20:26 +0000

    water simulation clean

--- src/water_simulation.cpp
+++ src/water_simulation.cpp
@@ -1,46 +1,68 @@
 #include "water_simulation.h"
 #include <godot_cpp/core/class_db.hpp>
 
+#include "global.h"
+
+#include <godot_cpp/classes/time.hpp>
+
+#include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+
+#include "water_domain.h"
+
+#define DO_NOT_PLAY_IN_EDITOR if(Engine::get_singleton()->is_editor_hint()) return
+#define PRINT(message) UtilityFunctions::print(message)
+
+
 using namespace godot;
 
-
-WaterSimulation::WaterSimulation() {
+WaterSimulation::WaterSimulation()
+{
 	active_domains = std::vector<WaterDomain*>();
 }
 
-WaterSimulation::~WaterSimulation() {
+WaterSimulation::~WaterSimulation()
+{
 	for (int i = 0; i < active_domains.size(); i++) {
 		delete active_domains[i];
 	}
 }
 
 
-NodePath WaterSimulation::get_terrain() const {
+NodePath WaterSimulation::get_terrain() const
+{
 	return terrain_node_path;
 }
 
-void WaterSimulation::set_terrain(NodePath nodePath) {
+void WaterSimulation::set_terrain(NodePath nodePath)
+{
 	terrain_node_path = nodePath;
 }
 
-NodePath WaterSimulation::get_water() const {
+NodePath WaterSimulation::get_water() const
+{
 	return water_node_path;
 }
 
-void WaterSimulation::set_water(NodePath nodePath) {
+void WaterSimulation::set_water(NodePath nodePath)
+{
 	water_node_path = nodePath;
 }
 
-int WaterSimulation::get_simulation_timestep() const {
+int WaterSimulation::get_simulation_timestep() const
+{
 	return simulation_timestep_mseconds;
 }
 
-void WaterSimulation::set_simulation_timestep(int _simulation_timestep_mseconds) {
+void WaterSimulation::set_simulation_timestep(int _simulation_timestep_mseconds)
+{
 	simulation_timestep_mseconds = _simulation_timestep_mseconds;
 }
 
 
-void WaterSimulation::_ready() {
+void WaterSimulation::_ready()
+{
 
 	DO_NOT_PLAY_IN_EDITOR;
 
@@ -60,7 +82,8 @@ void WaterSimulation::_ready() {
 
 }
 
-void WaterSimulation::_process(double delta) {
+void WaterSimulation::_process(double delta)
+{
 
 	DO_NOT_PLAY_IN_EDITOR;
 
@@ -98,99 +121,6 @@ void WaterSimulation::_process(double delta) {
 	// 		PRINT("Domain stable");
 	// 	}
 	// }
-}
-
-
-void WaterSimulation::update_domain(WaterDomain* domain) {
-
-	// const int CHANNEL = VoxelBuffer::CHANNEL_SDF;
-	// const float SPREAD_AMOUNT = 0.2;
-
-
-	// VoxelBuffer water_read_buffer = VoxelBuffer();
-	// water_read_buffer.create(domain->size.x, domain->size.y, domain->size.z);
-	// water_read_buffer.copy_channel_from(*domain->read_water_buffer, CHANNEL);
-
-	
-	// int voxels_per_level = (domain->size.x - 2) * (domain->size.z - 2);
-
-	// for (int y = Math::max(1, domain->stable_levels); y < domain->size.y; y++)
-	// {
-	// 	int stable_voxels_count = 0;
-
-	// 	for (int x = 1; x < domain->size.x - 1; x++)
-	// 	{
-	// 		for (int z = 1; z < domain->size.z - 1; z++)
-	// 		{
-				
-	// 			float water_voxel = water_read_buffer.get_voxel_f(x, y, z, CHANNEL);
-
-	// 			// If voxel has no water continue
-	// 			if (water_voxel >= 0) continue;
-
-	// 			float terrain_voxel_down = domain->read_terrain_buffer->ptr()->get_voxel_f(x, y - 1, z, CHANNEL);
-	// 			float water_voxel_down = water_read_buffer.get_voxel_f(x, y - 1, z, CHANNEL); // can go deeper
-
-
-	// 			if (terrain_voxel_down < 0)
-	// 			{
-	// 				// Spread
-	// 				std::array<float, 4> surr = domain->get_surr(water_read_buffer, x, y, z, CHANNEL);
-
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[0]) * SPREAD_AMOUNT, x + 1, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[1]) * SPREAD_AMOUNT, x, y, z + 1, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[2]) * SPREAD_AMOUNT, x - 1, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[3]) * SPREAD_AMOUNT, x, y, z - 1, CHANNEL);
-
-	// 				if (water_voxel < -.9)
-	// 				{
-	// 					stable_voxels_count += 1;
-	// 				}
-	// 			}
-	// 			else if (water_voxel_down < 0)
-	// 			{
-	// 				// Transfer down + spread
-
-	// 				// Amount of water to transfer to fill the voxel
-	// 				float transferred_water = 1.0 + water_voxel_down; // Water voxel down is negative
-	// 				//water_voxel += transferred_water; // ??
-
-	// 				domain->read_water_buffer->ptr()->set_voxel_f(water_voxel + transferred_water, x, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f(-1.0, x, y - 1, z, CHANNEL); // -1.0 = my_water(water_voxel_down) - transferred_water
-					
-	// 				std::array<float, 4> surr = domain->get_surr(water_read_buffer, x, y, z, CHANNEL);
-
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[0]) * SPREAD_AMOUNT, x + 1, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[1]) * SPREAD_AMOUNT, x, y, z + 1, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[2]) * SPREAD_AMOUNT, x - 1, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f((water_voxel - surr[3]) * SPREAD_AMOUNT, x, y, z - 1, CHANNEL);
-
-	// 				if (water_voxel < -.9 && water_voxel_down < -.9)
-	// 				{
-	// 					stable_voxels_count += 1;
-	// 				}
-	// 			}
-	// 			else
-	// 			{
-	// 				// Transfer down
-
-	// 				// Transfer all water
-	// 				domain->read_water_buffer->ptr()->set_voxel_f(1.0, x, y, z, CHANNEL);
-	// 				domain->read_water_buffer->ptr()->set_voxel_f(water_voxel, x, y - 1, z, CHANNEL);
-	// 			}
-	// 		}
-	// 	}
-
-	// 	if (stable_voxels_count == voxels_per_level)
-	// 	{
-	// 		domain->stable_levels += 1;
-	// 		PRINT("STABLE LEVEL");
-	// 	}
-	// }
-
-	// // domain->water->ptr()->fill_f(1, CHANNEL);
-	// water_tool.ptr()->paste(domain->origin, *domain->read_water_buffer, 1 << CHANNEL);
-
 }
 
 
--- src/water_simulation.h
+++ src/water_simulation.h
@@ -1,79 +1,68 @@
 #ifndef WATER_SIMULATION_H
 #define WATER_SIMULATION_H
 
-#include <vector>
-#include <godot_cpp/classes/voxel_generator_script.hpp>
-#include <godot_cpp/classes/voxel_buffer.hpp>
 #include <godot_cpp/classes/voxel_terrain.hpp>
-#include <godot_cpp/classes/voxel_tool.hpp>
-#include <godot_cpp/classes/time.hpp>
-#include <godot_cpp/variant/utility_functions.hpp>
-#include <godot_cpp/classes/engine.hpp>
-#include <math.h>
-#include "global.h"
-
-#include "water_domain.h"
 
 
-#define DO_NOT_PLAY_IN_EDITOR if(Engine::get_singleton()->is_editor_hint()) return
-#define PRINT(message) UtilityFunctions::print(message)
+namespace godot
+{
 
-namespace godot {
+	class WaterDomain;
+	class Time;
 
-class WaterSimulation : public Node {
-	GDCLASS(WaterSimulation, Node)
 
-private:
+	class WaterSimulation : public Node
+	{
+		GDCLASS(WaterSimulation, Node)
 
-	// This is a description
-	NodePath terrain_node_path;
-	NodePath water_node_path;
+	private:
 
-	VoxelTerrain* terrain;
-	VoxelTerrain* water;
+		NodePath terrain_node_path;
+		NodePath water_node_path;
 
-	Ref<VoxelTool> terrain_tool;
-	Ref<VoxelTool> water_tool;
+		VoxelTerrain* terrain;
+		VoxelTerrain* water;
 
-	Time* time;
-	int simulation_timestep_mseconds;
+		Ref<VoxelTool> terrain_tool;
+		Ref<VoxelTool> water_tool;
 
-	int last_step_time = 0;
+		Time* time;
+		int simulation_timestep_mseconds;
 
-	bool update_simulation;
+		int last_step_time = 0;
 
-	std::vector<WaterDomain*> active_domains;
+		bool update_simulation;
 
-	void update_domain(WaterDomain* domain);
+		std::vector<WaterDomain*> active_domains;
 
-protected:
-	static void _bind_methods();
+	protected:
+		static void _bind_methods();
 
-public:
-	WaterSimulation();
-	~WaterSimulation();
+	public:
+		WaterSimulation();
+		~WaterSimulation();
 
-	NodePath get_terrain() const;
-	void set_terrain(NodePath _terrain);
+		NodePath get_terrain() const;
+		void set_terrain(NodePath _terrain);
 
-	NodePath get_water() const;
-	void set_water(NodePath _water);
+		NodePath get_water() const;
+		void set_water(NodePath _water);
 
-	int get_simulation_timestep() const;
-	void set_simulation_timestep(int _simulation_timestep_mseconds);
+		int get_simulation_timestep() const;
+		void set_simulation_timestep(int _simulation_timestep_mseconds);
 
-	void _ready() override;
-	void _process(double delta) override;
+		void _ready() override;
+		void _process(double delta) override;
 
-	void add_domain(Vector3i origin, Vector3i size);
+		void add_domain(Vector3i origin, Vector3i size);
 
 
 
-	// GOOD
+		// GOOD
 
-	// void add_domain();
-	// void remove_domain();
-};
+		// void add_domain();
+		// void remove_domain();
+	};
 
 }
 
